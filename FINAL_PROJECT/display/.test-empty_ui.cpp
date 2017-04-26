ecf311816278ef332e6c265dc8e5c19da3c93f5f
Jacob Somera <jsome635@hawaii.edu>
2017-04-26 13:06:42 -1000

    Added test for empty ui

---
+++ FINAL_PROJECT/display/.test-empty_ui.cpp
@@ -0,0 +1,18 @@
+//	test-empty_ui.cpp
+//	
+//	EE205-002
+//	Final Project
+//	Tamagotchi
+//
+//	OG-Sans!!!
+//	Anthony Martin
+//	Charles-Rhon Rafael
+//	Jacob Somera
+//
+
+#include "display_functions.h"
+
+int main() {
+	empty_ui();
+	return 0;
+}
